a6feff683db72a2406964c26b3b817dccb09169b
Alejandro Rivera <rtalexk@gmail.com>
2017-07-24 20:14:32 -0600

    fix: remove unused device router

--- alicia_server/routes/device.router.js
+++ alicia_server/routes/device.router.js
@@ -4,9 +4,6 @@ module.exports = (wagner) => {
 
     const deviceController = require('../controllers/device.controller')(wagner.invoke((Device) => Device));
 
-    router.get('/', (req, res) => 
-        deviceController.getRegistrationArray(req, res));
-
     router.post('/', (req, res) => 
         deviceController.create(req, res));
 
